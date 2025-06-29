2a87762502effa62775646d7acfddee8429e2977
psandesh64 <psandesh64@gmail.com>
2025-06-29 20:31:59 +0545

    first commit

---
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -0,0 +1,59 @@
+﻿using Restaurants.Domain.Entities;
+using Restaurants.Infrastructure.Persistence;
+
+namespace Restaurants.Infrastructure.Seeders
+{
+    internal class RestaurantSeeder(RestaurantsDbContext dbContext) : IRestaurantSeeder
+    {
+        public async Task Seed()
+        {
+            if (await dbContext.Database.CanConnectAsync())
+            {
+                if (!dbContext.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    dbContext.Restaurants.AddRange(restaurants);
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+        }
+
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            List<Restaurant> restaurants = [
+                new()
+                {
+                    Name = "KFC",
+                    Category = "Fast Food",
+                    Description = "KFC is an AMerican food Company",
+                    ContactEmail = "caont@yahoo.com",
+                    HasDelivery = true,
+                    Dishes =
+                    [
+                        new()
+                        {
+                            Name = "Nashville Hot Chicken",
+                            Description = "Nash (10pcs)",
+                            Price = 10.30M,
+                        },
+                        new()
+                        {
+                            Name = "Chicken Nuggets",
+                            Description = "Nuggets (5pcs)",
+                            Price = 5.30M,
+                        },
+                    ],
+                    Address = new()
+                    {
+                        City = "London",
+                        Street = "Cork St 5",
+                        PostalCode = "WC2N 5DU",
+                    }
+                },
+
+            ];
+            return restaurants;
+        }
+
+    }
+}
---
+++ Restaurants/Program.cs
@@ -0,0 +1,39 @@
+using Restaurants.Controllers;
+using Restaurants.Infrastructure.Extensions;
+using Restaurants.Infrastructure.Seeders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
+
+builder.Configuration.GetConnectionString("RestaurantsDb");
+builder.Services.AddInfrastructure(builder.Configuration);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+
+await seeder.Seed();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
